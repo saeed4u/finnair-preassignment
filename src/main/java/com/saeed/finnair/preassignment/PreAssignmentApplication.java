027c5a5843acd38beedb647cfab5efa8e0249a5b
= <=>
2019-10-01 22:16:24 +0300

    First commit with project main dependencies

---
+++ src/main/java/com/saeed/finnair/preassignment/PreAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.saeed.finnair.preassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreAssignmentApplication.class, args);
+	}
+
+}
